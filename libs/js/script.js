37f9f4cb16b463eecc8e52b3cee1ee7990cee8b3
Nator1111 <james.brierley19@gmail.com>
2021-01-18 18:34:53 +0000

    Second Commit

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,11 +1,3 @@
-/*Choose a Random Card*/
-
-/*function randomCard(array) {
-    for(let i = 0; i < array.length; i++){
-        Math.random()
-    }
-}*/
-
 /*On Page Load, Randomise cards and Render value to screen*/
 
 $(document).ready(function() {
@@ -19,12 +11,60 @@ $(document).ready(function() {
 
             //console.log(result);
 
+            /*Initialise Array and Score/Lives Count */
+
+            let cardArray = [];
             let score = 0;
             let lives = 3;
 
-            $('#score').append(`Score = ${score} Lives = ${lives}`);
+            /*Choose a Random Card*/
 
-            $('#card').append(`${result[6]['value']}`);
+            function shuffle(a) {
+                for (let i = a.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [a[i], a[j]] = [a[j], a[i]];
+                }
+                cardArray.push(a[0]);
+            }
+
+            shuffle(result)
+
+            $('#score').append(`Score = ${score} Lives = ${lives}`);
+            
+            /*Setup with random number*/
+
+            $('#card').append(`${cardArray[0]['value']} ${cardArray[0]['suit']}`);  
+
+            $('#higher').on('click', function() {
+
+                $.ajax({
+                    url: "libs/php/getCards.php",
+                    type: 'GET',
+                    dataType: 'json',
+            
+                    success: function(result) {
+            
+                        shuffle(result);
+
+                        console.log(cardArray[cardArray.length -1]);
+
+                        $('#score').empty();
+                        $('#card').empty();
+
+                        if(cardArray[cardArray.length -1]['value'] > cardArray[cardArray.length -2]['value']){
+                            score += 1;
+                        } else {
+                            lives -= 1;
+                        }
+                        
+                        $('#score').append(`Score = ${score} Lives = ${lives}`);
+                        $('#card').append(`${cardArray[cardArray.length -1]['value']} ${cardArray[cardArray.length -1]['suit']}`);  
+                        
+                        
+                    }
+                })
+                
+            });
             
         }
     })
